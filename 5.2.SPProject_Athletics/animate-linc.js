f08321ecaf38380de9d2ed27edc3fccf6802f58c
The Temple News <web@temple-news.com>
2019-05-01 21:49:13 -0400

    Create animate-linc.js

---
+++ 5.2.SPProject_Athletics/animate-linc.js
@@ -0,0 +1,116 @@
+function game1(){
+    
+    for (var i = 1; i <= 120; i++){
+        document.getElementById(i).style.fill = "#e5667e";
+    }
+    
+    for (var i = 1; i <= 25; i++){
+        document.getElementById(i).style.fill = "#9E1B34";
+    }
+
+    
+    document.getElementById("team").textContent = "Vs. Villanova";
+    document.getElementById("date").textContent = "September 1, 2018";
+    document.getElementById("tickets-sold").innerHTML = document.getElementById("tickets-sold").innerHTML.slice(0,14) + "<b>32,357</b>";
+    document.getElementById("actual-attendance").innerHTML = document.getElementById("actual-attendance").innerHTML.slice(0,20) + "<b>3845</b>";
+
+    setTimeout(game2, 3000);
+    
+
+}
+
+function game2(){
+
+    for (var i = 1; i <= 93; i++){
+        document.getElementById(i).style.fill = "#e5667e";
+    }
+    
+    for (var i = 1; i <= 16; i++){
+        document.getElementById(i).style.fill = "#9E1B34";
+    }
+
+    document.getElementById("team").textContent = "Vs. University of Buffalo";
+    document.getElementById("date").textContent = "September 8, 2018";
+    document.getElementById("tickets-sold").innerHTML = document.getElementById("tickets-sold").innerHTML.slice(0,14) + "<b>25,511</b>";
+    document.getElementById("actual-attendance").innerHTML = document.getElementById("actual-attendance").innerHTML.slice(0,20) + "<b>2,230</b>";
+
+    setTimeout(game3, 3000);
+
+}
+
+function game3(){
+
+    for (var i = 1; i <= 92; i++){
+        document.getElementById(i).style.fill = "#e5667e";
+    }
+    
+    for (var i = 1; i <= 18; i++){
+        document.getElementById(i).style.fill = "#9E1B34";
+    }
+
+    document.getElementById("team").textContent = "Vs. Tulsa";
+    document.getElementById("date").textContent = "September 20, 2018";
+    document.getElementById("tickets-sold").innerHTML = document.getElementById("tickets-sold").innerHTML.slice(0,14) + "<b>24,217</b>";
+    document.getElementById("actual-attendance").innerHTML = document.getElementById("actual-attendance").innerHTML.slice(0,20) + "<b>2,171</b>";
+
+    setTimeout(game4, 3000);
+
+}
+
+function game4(){
+
+    for (var i = 1; i <= 96; i++){
+        document.getElementById(i).style.fill = "#e5667e";
+    }
+    
+    for (var i = 1; i <= 8; i++){
+        document.getElementById(i).style.fill = "#9E1B34";
+    }
+
+    document.getElementById("team").textContent = "Vs. East Carolina";
+    document.getElementById("date").textContent = "October 6, 2018";
+    document.getElementById("tickets-sold").innerHTML = document.getElementById("tickets-sold").innerHTML.slice(0,14) + "<b>26,681</b>";
+    document.getElementById("actual-attendance").innerHTML = document.getElementById("actual-attendance").innerHTML.slice(0,20) + "<b>1,228</b>";
+
+    setTimeout(game5, 3000);
+
+}
+
+function game5(){
+
+    for (var i = 1; i <= 97; i++){
+        document.getElementById(i).style.fill = "#e5667e";
+    }
+    
+    for (var i = 1; i <= 15; i++){
+        document.getElementById(i).style.fill = "#9E1B34";
+    }
+
+    document.getElementById("team").textContent = "Vs. Cincinnati";
+    document.getElementById("date").textContent = "October 20, 2018";
+    document.getElementById("tickets-sold").innerHTML = document.getElementById("tickets-sold").innerHTML.slice(0,14) + "<b>33,026</b>";
+    document.getElementById("actual-attendance").innerHTML = document.getElementById("actual-attendance").innerHTML.slice(0,20) + "<b>3,272</b>";
+
+    setTimeout(game6, 3000);
+
+}
+
+function game6(){
+
+    for (var i = 1; i <= 114; i++){
+        document.getElementById(i).style.fill = "#e5667e";
+    }
+    
+    for (var i = 1; i <= 5; i++){
+        document.getElementById(i).style.fill = "#9E1B34";
+    }
+
+    document.getElementById("team").textContent = "Vs. South Florida";
+    document.getElementById("date").textContent = "November 17, 2018";
+    document.getElementById("tickets-sold").innerHTML = document.getElementById("tickets-sold").innerHTML.slice(0,14) + "<b>29,029</b>";
+    document.getElementById("actual-attendance").innerHTML = document.getElementById("actual-attendance").innerHTML.slice(0,20) + "<b>520</b>";
+
+    setTimeout(game1, 3000);
+
+}
+
