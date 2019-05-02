85312b3a115db47c05e0c3f16913768c71579427
The Temple News <web@temple-news.com>
2019-05-01 21:48:34 -0400

    Create animate-liacouras.js

---
+++ 5.2.SPProject_Athletics/animate-liacouras.js
@@ -0,0 +1,286 @@
+
+function game1(){
+    
+    for (var i = 1; i <= 120; i++){
+        document.getElementById("cir-"+i).style.fill = "#e5667e";
+    }
+    
+    for (var i = 1; i <= 25; i++){
+        document.getElementById("cir-"+i).style.fill = "#9E1B34";
+    }
+
+    
+    document.getElementById("team").textContent = "Vs. La Salle";
+    document.getElementById("date").textContent = "November 6, 2018";
+    document.getElementById("tickets-sold").innerHTML = document.getElementById("tickets-sold").innerHTML.slice(0,14) + "<b>6,011</b>";
+    document.getElementById("actual-attendance").innerHTML = document.getElementById("actual-attendance").innerHTML.slice(0,20) + "<b>1,252</b>";
+
+    setTimeout(game2, 3000);
+    
+
+}
+
+function game2(){
+
+    for (var i = 1; i <= 93; i++){
+        document.getElementById("cir-"+i).style.fill = "#e5667e";
+    }
+    
+    for (var i = 1; i <= 16; i++){
+        document.getElementById("cir-"+i).style.fill = "#9E1B34";
+    }
+
+    document.getElementById("team").textContent = "Vs. University of Detroit Mercy";
+    document.getElementById("date").textContent = "November 9, 2018";
+    document.getElementById("tickets-sold").innerHTML = document.getElementById("tickets-sold").innerHTML.slice(0,14) + "<b>4,649</b>";
+    document.getElementById("actual-attendance").innerHTML = document.getElementById("actual-attendance").innerHTML.slice(0,20) + "<b>824</b>";
+
+    setTimeout(game3, 3000);
+
+}
+
+function game3(){
+
+    for (var i = 1; i <= 92; i++){
+        document.getElementById("cir-"+i).style.fill = "#e5667e";
+    }
+    
+    for (var i = 1; i <= 18; i++){
+        document.getElementById("cir-"+i).style.fill = "#9E1B34";
+    }
+
+    document.getElementById("team").textContent = "Vs. University of Georgia";
+    document.getElementById("date").textContent = "November 13, 2018";
+    document.getElementById("tickets-sold").innerHTML = document.getElementById("tickets-sold").innerHTML.slice(0,14) + "<b>4,613</b>";
+    document.getElementById("actual-attendance").innerHTML = document.getElementById("actual-attendance").innerHTML.slice(0,20) + "<b>907</b>";
+
+    setTimeout(game4, 3000);
+
+}
+
+function game4(){
+
+    for (var i = 1; i <= 96; i++){
+        document.getElementById("cir-"+i).style.fill = "#e5667e";
+    }
+    
+    for (var i = 1; i <= 8; i++){
+        document.getElementById("cir-"+i).style.fill = "#9E1B34";
+    }
+
+    document.getElementById("team").textContent = "Vs. Loyola University Maryland";
+    document.getElementById("date").textContent = "November 16, 2018";
+    document.getElementById("tickets-sold").innerHTML = document.getElementById("tickets-sold").innerHTML.slice(0,14) + "<b>4,815</b>";
+    document.getElementById("actual-attendance").innerHTML = document.getElementById("actual-attendance").innerHTML.slice(0,20) + "<b>390</b>";
+
+    setTimeout(game5, 3000);
+
+}
+
+function game5(){
+
+    for (var i = 1; i <= 97; i++){
+        document.getElementById("cir-"+i).style.fill = "#e5667e";
+    }
+    
+    for (var i = 1; i <= 15; i++){
+        document.getElementById("cir-"+i).style.fill = "#9E1B34";
+    }
+
+    document.getElementById("team").textContent = "Vs. University of Massachusetts Amherst";
+    document.getElementById("date").textContent = "December 12, 2018";
+    document.getElementById("tickets-sold").innerHTML = document.getElementById("tickets-sold").innerHTML.slice(0,14) + "<b>4,840</b>";
+    document.getElementById("actual-attendance").innerHTML = document.getElementById("actual-attendance").innerHTML.slice(0,20) + "<b>759</b>";
+
+    setTimeout(game6, 3000);
+
+}
+
+function game6(){
+
+    for (var i = 1; i <= 114; i++){
+        document.getElementById("cir-"+i).style.fill = "#e5667e";
+    }
+    
+    for (var i = 1; i <= 5; i++){
+        document.getElementById("cir-"+i).style.fill = "#9E1B34";
+    }
+
+    document.getElementById("team").textContent = "Vs. Houston";
+    document.getElementById("date").textContent = "January 9, 2019";
+    document.getElementById("tickets-sold").innerHTML = document.getElementById("tickets-sold").innerHTML.slice(0,14) + "<b>5,723</b>";
+    document.getElementById("actual-attendance").innerHTML = document.getElementById("actual-attendance").innerHTML.slice(0,20) + "<b>238</b>";
+
+    setTimeout(game7, 3000);
+
+}
+
+function game7(){
+
+    for (var i = 1; i <= 137; i++){
+        document.getElementById("cir-"+i).style.fill = "#e5667e";
+    }
+    
+    for (var i = 1; i <= 5; i++){
+        document.getElementById("cir-"+i).style.fill = "#9E1B34";
+    }
+
+    document.getElementById("team").textContent = "Vs. South Florida";
+    document.getElementById("date").textContent = "January 12, 2019";
+    document.getElementById("tickets-sold").innerHTML = document.getElementById("tickets-sold").innerHTML.slice(0,14) + "<b>6,826</b>";
+    document.getElementById("actual-attendance").innerHTML = document.getElementById("actual-attendance").innerHTML.slice(0,20) + "<b>266</b>";
+
+    setTimeout(game8, 3000);
+    
+}
+
+function game8(){
+
+    for (var i = 1; i <= 168; i++){
+        document.getElementById("cir-"+i).style.fill = "#e5667e";
+    }
+    
+    for (var i = 1; i <= 31; i++){
+        document.getElementById("cir-"+i).style.fill = "#9E1B34";
+    }
+    document.getElementById("team").textContent = "Vs. Penn";
+    document.getElementById("date").textContent = "January 19, 2019";
+    document.getElementById("tickets-sold").innerHTML = document.getElementById("tickets-sold").innerHTML.slice(0,14) + "<b>8,409</b>";
+    document.getElementById("actual-attendance").innerHTML = document.getElementById("actual-attendance").innerHTML.slice(0,20) + "<b>1,562</b>";
+
+    setTimeout(game9, 3000);
+
+}
+
+function game9(){
+
+    for (var i = 1; i <= 107; i++){
+        document.getElementById("cir-"+i).style.fill = "#e5667e";
+    }
+    
+    for (var i = 1; i <= 17; i++){
+        document.getElementById("cir-"+i).style.fill = "#9E1B34";
+    }
+
+    document.getElementById("team").textContent = "Vs. Memphis";
+    document.getElementById("date").textContent = "January 24, 2019";
+    document.getElementById("tickets-sold").innerHTML = document.getElementById("tickets-sold").innerHTML.slice(0,14) + "<b>5,364</b>";
+    document.getElementById("actual-attendance").innerHTML = document.getElementById("actual-attendance").innerHTML.slice(0,20) + "<b>863</b>";
+
+    setTimeout(game10, 3000);
+
+}
+
+function game10(){
+
+    for (var i = 1; i <= 133; i++){
+        document.getElementById("cir-"+i).style.fill = "#e5667e";
+    }
+    
+    for (var i = 1; i <= 12; i++){
+        document.getElementById("cir-"+i).style.fill = "#9E1B34";
+    }
+
+    document.getElementById("team").textContent = "Vs. Cincinnati";
+    document.getElementById("date").textContent = "January 27, 2019";
+    document.getElementById("tickets-sold").innerHTML = document.getElementById("tickets-sold").innerHTML.slice(0,14) + "<b>6,652</b>";
+    document.getElementById("actual-attendance").innerHTML = document.getElementById("actual-attendance").innerHTML.slice(0,20) + "<b>612</b>";
+
+    setTimeout(game11, 3000);
+
+}
+
+function game11(){
+
+    for (var i = 1; i <= 123; i++){
+        document.getElementById("cir-"+i).style.fill = "#e5667e";
+    }
+    
+    for (var i = 1; i <= 13; i++){
+        document.getElementById("cir-"+i).style.fill = "#9E1B34";
+    }
+
+    document.getElementById("team").textContent = "Vs. UConn";
+    document.getElementById("date").textContent = "February 6, 2019";
+    document.getElementById("tickets-sold").innerHTML = document.getElementById("tickets-sold").innerHTML.slice(0,14) + "<b>6,129</b>";
+    document.getElementById("actual-attendance").innerHTML = document.getElementById("actual-attendance").innerHTML.slice(0,20) + "<b>672</b>";
+
+    setTimeout(game12, 3000);
+
+}
+
+function game12(){
+
+    for (var i = 1; i <= 98; i++){
+        document.getElementById("cir-"+i).style.fill = "#e5667e";
+    }
+    
+    for (var i = 1; i <= 21; i++){
+        document.getElementById("cir-"+i).style.fill = "#9E1B34";
+    }
+
+    document.getElementById("team").textContent = "Vs. Southern Methodist";
+    document.getElementById("date").textContent = "February 13, 2019";
+    document.getElementById("tickets-sold").innerHTML = document.getElementById("tickets-sold").innerHTML.slice(0,14) + "<b>4,913</b>";
+    document.getElementById("actual-attendance").innerHTML = document.getElementById("actual-attendance").innerHTML.slice(0,20) + "<b>1,066</b>";
+
+    setTimeout(game13, 3000);
+
+}
+
+function game13(){
+
+    for (var i = 1; i <= 144; i++){
+        document.getElementById("cir-"+i).style.fill = "#e5667e";
+    }
+    
+    for (var i = 1; i <= 14; i++){
+        document.getElementById("cir-"+i).style.fill = "#9E1B34";
+    }
+
+    document.getElementById("team").textContent = "Vs. Tulsa";
+    document.getElementById("date").textContent = "February 23, 2019";
+    document.getElementById("tickets-sold").innerHTML = document.getElementById("tickets-sold").innerHTML.slice(0,14) + "<b>7,183</b>";
+    document.getElementById("actual-attendance").innerHTML = document.getElementById("actual-attendance").innerHTML.slice(0,20) + "<b>709</b>";
+
+    setTimeout(game14, 3000);
+
+}
+
+function game14(){
+
+    for (var i = 1; i <= 142; i++){
+        document.getElementById("cir-"+i).style.fill = "#e5667e";
+    }
+    
+    for (var i = 1; i <= 3; i++){
+        document.getElementById("cir-"+i).style.fill = "#9E1B34";
+    }
+
+    document.getElementById("team").textContent = "Vs. Tulane";
+    document.getElementById("date").textContent = "March 3, 2019";
+    document.getElementById("tickets-sold").innerHTML = document.getElementById("tickets-sold").innerHTML.slice(0,14) + "<b>7,090</b>";
+    document.getElementById("actual-attendance").innerHTML = document.getElementById("actual-attendance").innerHTML.slice(0,20) + "<b>144</b>";
+
+    setTimeout(game15, 3000);
+
+}
+
+function game15(){
+
+    for (var i = 1; i <= 199; i++){
+        document.getElementById("cir-"+i).style.fill = "#e5667e";
+    }
+    
+    for (var i = 1; i <= 11; i++){
+        document.getElementById("cir-"+i).style.fill = "#9E1B34";
+    }
+
+    document.getElementById("team").textContent = "Vs. Central Florida";
+    document.getElementById("date").textContent = "March 9, 2019";
+    document.getElementById("tickets-sold").innerHTML = document.getElementById("tickets-sold").innerHTML.slice(0,14) + "<b>9,951</b>";
+    document.getElementById("actual-attendance").innerHTML = document.getElementById("actual-attendance").innerHTML.slice(0,20) + "<b>569</b>";
+
+    setTimeout(game1, 3000);
+
+}
